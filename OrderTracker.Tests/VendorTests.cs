c1572747d38810859877bf9595aa928d1d2a37a7
MarielHamson <45244997+MarielHamson@users.noreply.github.com>
2020-07-24 09:53:04 -0700

    fail test getting name and description properties

--- OrderTracker.Tests/VendorTests.cs
+++ OrderTracker.Tests/VendorTests.cs
@@ -15,8 +15,33 @@ namespace OrderTracker.Tests
     [TestMethod]
     public void VendorConstructor_CreatesInstanceOfVendor_Vendor()
     {
-      Vendor newVendor = new Vendor();
+      Vendor newVendor = new Vendor("Suzy's Boulangerie", "A bakery downtown");
       Assert.AreEqual(typeof(Vendor), newVendor.GetType());
+    }
+
+    [TestMethod]
+    public void GetName_ReturnsName_String()
+    {
+      string name = "Suzy's Boulangerie";
+      string description = "A bakery downtown";
+      Vendor newVendor = new Vendor(name, description);
+      string nameResult = "test name";
+      string descriptionResult = "test description";
+      Assert.AreEqual(name, nameResult);
+      Assert.AreEqual(description, descriptionResult);
+    }
+
+    [TestMethod]
+    public void getDescription_ReturnsDescription_String()
+    {
+      string name = "Suzy's Boulangerie";
+      string description = "A bakery downtown";
+      Vendor newVendor = new Vendor(name, description);
+      string descriptionResult = "test description";
+      Assert.AreEqual(description, descriptionResult);
+
+
+
     }
   }
 }
--- OrderTracker/Models/Vendor.cs
+++ OrderTracker/Models/Vendor.cs
@@ -3,6 +3,17 @@ namespace OrderTracker.Models
 {
   public class Vendor
   {
+    private static List<Vendor> _instances = new List<Vendor> { };
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public List<Order> Orders { get; set; }
+    public Vendor(string vendorName, string description)
+    {
+      Name = vendorName;
+      Description = description;
+      _instances.Add(this);
+      Orders = new List<Order> { };
+    }
 
     // properties, methods, etc. will go here.
 
