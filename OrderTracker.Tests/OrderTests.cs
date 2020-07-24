9619cec51c9d92d91b3e915a8e74ed1b8bc77916
MarielHamson <45244997+MarielHamson@users.noreply.github.com>
2020-07-24 08:52:14 -0700

    pass test for Order constructor

--- OrderTracker.Tests/OrderTests.cs
+++ OrderTracker.Tests/OrderTests.cs
@@ -1,13 +1,23 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using OrderTracker.Models;
+using System.Collections.Generic;
+using System;
 
 namespace OrderTracker.Tests
 {
   [TestClass]
-  public class OrderTests
+  public class OrderTests //: IDisposable
   {
+    // public void Dispose()
+    // {
+    //   Order.ClearAll();
+    // }
 
-    // Test methods will go here.
-
+    [TestMethod]
+    public void OrderConstructor_CreatesInstanceOfOrder_Order()
+    {
+      Order newOrder = new Order();
+      Assert.AreEqual(typeof(Order), newOrder.GetType());
+    }
   }
 }
---
+++ OrderTracker/Models/Order.cs
@@ -0,0 +1,14 @@
+using System.Collections.Generic;
+
+namespace OrderTracker.Models
+{
+  public class Order
+  {
+
+
+    // public static void ClearAll()
+    // {
+    //   _instances.Clear();
+    // }
+  }
+}
