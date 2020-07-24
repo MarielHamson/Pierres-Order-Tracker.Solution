b856cfce7a8759c0417dd41623b702c16bec4105
MarielHamson <45244997+MarielHamson@users.noreply.github.com>
2020-07-24 10:33:59 -0700

    add orders controller

--- OrderTracker/Controllers/OrdersController.cs
+++ OrderTracker/Controllers/OrdersController.cs
@@ -6,5 +6,29 @@ namespace OrderTracker.Controllers
 {
   public class OrdersController : Controller
   {
+    [HttpGet("/VendorsController/{vendorId}/orders/new")]
+    public Actionresult New(int vendorId)
+    {
+      Vendor vendor = Vendor.Find(vendorId);
+      return View(vendor);
+    }
+
+    [HttpGet("/vendors/{vendorId}/orders/{orderId}")]
+    public ActionResult Show(int vendorId, int orderId)
+    {
+      Order order = Order.Find(orderId);
+      Vendor vendor = Vendor.Find(vendorId);
+      Dictionary<string, object> model = new Dictionary<string, object>();
+      model.Add("order", order);
+      model.Add("vendor", vendor);
+      return View(model);
+    }
+
+    [HttpPost("/orders/delete")]
+    public ActionResult Delete All()
+    {
+      Order.ClearAll();
+      return View();
+    }
   }
 }
